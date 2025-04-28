1e719d6a57ae60eeff3bde202c5423c57e95a127
Dinarkhansas <salsabiladinarkhansa@gmail.com>
2025-04-28 18:05:28 +0700

    Kelompok 6

--- course_service.py
+++ course_service.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 courses = {
     301: {"id": 301, "name": "Pemrograman Web", "sks": 3},
     302: {"id": 302, "name": "Sistem Operasi", "sks": 4},
+    303: {"id": 303, "name": "Struktur Data", "sks": 3},
+    304: {"id": 304, "name": "Kecerdasan Buatan", "sks": 3},
+    305: {"id": 305, "name": "Jaringan Komputer", "sks": 3},
+    306: {"id": 306, "name": "Pengolahan Citra Digital", "sks": 2},
+    307: {"id": 307, "name": "Pemrograman Mobile", "sks": 3},
+    308: {"id": 308, "name": "Etika Profesi", "sks": 2},
 }
 
 @app.route("/courses/<int:course_id>")
--- student_service.py
+++ student_service.py
@@ -1,3 +1,4 @@
+
 # student_service.py
 from flask import Flask, jsonify
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 students = {
     101: {"id": 101, "name": "Aulia Fajar", "major": "Informatika"},
     102: {"id": 102, "name": "Sinta Dewi", "major": "Sistem Informasi"},
+    103: {"id": 103, "name": "Budi Santoso", "major": "Teknik Komputer"},
+    104: {"id": 104, "name": "Citra Lestari", "major": "Teknik Informatika"},
+    105: {"id": 105, "name": "Dian Puspita", "major": "Manajemen Informatika"},
+    106: {"id": 106, "name": "Eko Prasetyo", "major": "Sains Data"},
+    107: {"id": 107, "name": "Fajar Nugroho", "major": "Sistem Komputer"},
+    108: {"id": 108, "name": "Gita Sari", "major": "Rekayasa Perangkat Lunak"},
 }
 
 @app.route("/students/<int:student_id>")
